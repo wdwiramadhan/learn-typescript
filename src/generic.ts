aa13c4dc4a6d84eaf44979db5901d9d91385e0e0
wdwiramadhan <wahyudwiramadhan@gmail.com>
2020-06-18 14:15:01 +0700

    add generic and enum

--- src/generic.ts
+++ src/generic.ts
@@ -1,8 +1,33 @@
-const addUUid = (obj:object) => {
+const addUUid = <T extends {} >(obj:T) => {
   let uid = Math.floor(Math.random()*10000)
   return {...obj,uid}
 }
 
 let docOne = addUUid({name:"andi",age:12})
+let docTwo = addUUid({name:'yayan'})
 
 console.log(docOne)
+console.log(docTwo)
+
+//enum 
+enum ResourceType{BOOK, AUTHOR, FILM, PERSON}
+//generic
+interface Resource<T>{
+  uid:number;
+  resourceType:ResourceType;
+  data:T;
+}
+
+const docThree:Resource<number> = {
+  uid:12,
+  resourceType:ResourceType.AUTHOR,
+  data: 12
+}
+
+const docFour: Resource<string> = {
+  uid : 14,
+  resourceType : ResourceType.PERSON,
+  data : 'not found'
+}
+console.log(docThree)
+console.log(docFour)
